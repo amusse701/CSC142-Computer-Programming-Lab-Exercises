01aede70879e49dca7a5b764f13e76aed30f57f4
Abdullahi Musse <amusse701@gmail.com>
2025-07-21 21:16:04 -0700

    add project 1 program and screenshot

---
+++ project_1/p1.java
@@ -0,0 +1,8 @@
+package project_1;
+
+public class p1 {
+	public static void main(String[] args) {
+		System.out.println("CSC142 Intro to Java");
+		System.out.println("One thing i learned from module one is how to write my first java program ");
+	}
+}
